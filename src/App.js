ea4a26735f6cdaa0187de2270a09cb1738cd59f3
Ivaylo-Kirov <ivo.kirov@gmail.com>
2019-08-25 14:46:46 -0400

    Initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {useQuery} from '@apollo/react-hooks';
+import {gql} from 'apollo-boost';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const {loading, error, data} = useQuery(
+    gql`
+    {
+      rates(currency:"USD") {
+        currency
+        rate
+      }
+    }
+    `
   );
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+  return data.rates.map(({currency, rate}) => (
+        <div key={currency}>
+          <p>
+            {currency}: {rate}
+          </p>
+        </div>
+  ));
 }
 
 export default App;
